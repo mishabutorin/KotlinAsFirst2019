107e0e9b30884814e137f168ec683ad5ac4ee677
mishabutorin <55362341+mishabutorin@users.noreply.github.com>
2020-03-08 22:59:39 +0300

    do 11 lesson

--- test/lesson11/task1/ComplexTest.kt
+++ test/lesson11/task1/ComplexTest.kt
@@ -23,17 +23,17 @@ internal class ComplexTest {
 
     @Test
     fun minus() {
-        assertApproxEquals(Complex("4-2i"), Complex("1+2i") + Complex("3-4i"), 1e-10)
+        assertApproxEquals(Complex("1+6i"), Complex("4+2i") - Complex("3-4i"), 1e-10)
     }
 
     @Test
     fun times() {
-        assertApproxEquals(Complex("11-8i"), Complex("1+2i") + Complex("3-4i"), 1e-10)
+        assertApproxEquals(Complex("11+2i"), Complex("1+2i") * Complex("3-4i"), 1e-10)
     }
 
     @Test
     fun div() {
-        assertApproxEquals(Complex("1+2i"), Complex("11-8i") + Complex("3-4i"), 1e-10)
+        assertApproxEquals(Complex("2.230769230769231+0.15384615384615385i"), Complex("7-4i") / Complex("3-2i"), 1e-10)
     }
 
     @Test
